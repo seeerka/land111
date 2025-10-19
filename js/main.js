f418413e9e0de8c7c85590a77a6e7612311ccf8c
seeerka <sseeerka@bk.ru>
2025-10-19 15:20:19 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,173 @@
+"use strict"
+
+document.documentElement.classList.add("loaded"); // когда страница загрузилась
+
+window.addEventListener("DOMContentLoaded", windowLoad); // для анимации для первых экранов, чтобы ничего не дергалось
+
+function windowLoad() {}; // для анимации для первых экранов, чтобы ничего не дергалось
+
+
+// Открытие и закрытие бургера
+
+const menu = document.querySelector(".header__menu");
+
+document.addEventListener("click", function (e) {
+
+  const elementInteractive = e.target;
+
+  if (elementInteractive.closest(".burger")) { // Открытие и закрытие бургера
+    menu.classList.add("active")
+    document.body.style.overflow = "hidden";
+  }
+  if (elementInteractive.closest(".header__exit-menu")) { // Открытие и закрытие бургера 
+    menu.classList.remove("active")
+    document.body.style.overflow = "";
+  }
+  if (elementInteractive.closest(".nav__link")) { // Открытие и закрытие бургера
+    menu.classList.remove("active")
+    document.body.style.overflow = "";
+  }
+  if (elementInteractive.closest(".faq__details summary")) {
+
+  const faqDetails = elementInteractive.closest(".faq__details"); 
+
+  if (!faqDetails.classList.contains("active")) {  
+
+    faqDetails.classList.add("active"); 
+
+  } else if (faqDetails.classList.contains("active")) { 
+
+      faqDetails.classList.remove("active"); 
+  }
+
+}
+})
+
+// Плагин загрузки изображений и видео
+
+const rdUpload = document.querySelectorAll(".rd-upload");
+
+const body = document.body;
+
+if (rdUpload) {
+  const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0,
+  }
+
+  const callback = function (entries, observer) {
+
+    entries.forEach(entry => {
+      const elementEntry = entry.target;
+      if (entry.isIntersecting) {
+
+        if (!elementEntry.classList.contains("rd-load")) {
+          elementEntry.classList.add("rd-load");
+
+          let rdUploadBackgroundImage = elementEntry.querySelectorAll(".rd-background");
+
+          let rdUploadImage = elementEntry.querySelectorAll("img");
+
+          let rdUploadVideo = elementEntry.querySelectorAll("video");
+
+          let rdUploadPicture = elementEntry.querySelectorAll("picture");
+
+          if (elementEntry.classList.contains("rd-background")) {
+
+            let elementEntryBackgroundImageDataImage = elementEntry.dataset.rdImage;
+
+            elementEntry.style.backgroundImage = `url(${elementEntryBackgroundImageDataImage})`;
+          }
+
+          if (rdUploadBackgroundImage) {
+            rdUploadBackgroundImage.forEach(function (rdUploadBackgroundImage) {
+
+              let rdUploadBackgroundImageDataImage = rdUploadBackgroundImage.dataset.rdImage;
+
+              rdUploadBackgroundImage.style.backgroundImage = `url(${rdUploadBackgroundImageDataImage})`;
+
+            })
+          }
+
+          if (rdUploadImage) {
+            rdUploadImage.forEach(function (rdUploadImage) {
+
+              if (!rdUploadImage.closest("picture")) {
+                let rdUploadImageDataImage = rdUploadImage.dataset.rdImage;
+
+                rdUploadImage.setAttribute("src", `${rdUploadImageDataImage}`);
+              }
+
+            })
+          }
+
+          if (rdUploadPicture) {
+            rdUploadPicture.forEach(function (rdUploadPicture) {
+
+              let rdUploadPictureSource = rdUploadPicture.querySelectorAll("source");
+
+              rdUploadPictureSource.forEach(function (rdUploadPictureSource) {
+
+                let rdUploadPictureSourceImage = rdUploadPictureSource.dataset.rdImage;
+
+                rdUploadPictureSource.setAttribute("srcset", `${rdUploadPictureSourceImage}`);
+
+              })
+
+            })
+          }
+
+          if (rdUploadVideo) {
+            rdUploadVideo.forEach(function (rdUploadVideo) {
+
+              let rdUploadVideoDataVideo = rdUploadVideo.dataset.rdVideo;
+              let rdUploadVideoDataImage = rdUploadVideo.dataset.rdImage;
+
+              rdUploadVideo.setAttribute("src", `${rdUploadVideoDataVideo}`);
+
+              rdUploadVideo.setAttribute("poster", `${rdUploadVideoDataImage}`);
+
+            })
+          }
+        }
+
+      }
+
+    })
+
+  }
+
+  const observer = new IntersectionObserver(callback, options);
+
+  rdUpload.forEach(i => {
+    observer.observe(i);
+  })
+}
+  
+const bonusSlider = document.querySelector(".bonus-slider");
+
+if (bonusSlider) {
+  const bonusSwiper = new Swiper(bonusSlider, {
+    observer: true,
+    observeParents: true,
+    watchOverflow: true,
+    slidesPerView: 'auto',
+    spaceBetween: 32,
+    direction: 'horizontal',
+    effect: 'flip',
+    pagination: {
+      el: '.bonus-slider-pagination',
+      clickable: true,
+    },
+  });
+}
+const reviewsItem = document.querySelectorAll(".reviews__item");
+
+if (document.body.clientWidth < 992) {
+  if(reviewsItem) {
+    reviewsItem.forEach(function(e) {
+      e.append(e.querySelector(".reviews__link"));
+    })
+  } 
+}
